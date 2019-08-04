e2268976214d5c38bee458a7c6eb5cd23a5e68ff
supavadi <supavadi_bell@kjst.ac.th>
2019-08-04 15:06:28 +0700

    fonten

---
+++ script.js
@@ -0,0 +1,9 @@
+// //collapse nav links and .navbar-brand on mobile
+// $('.navbar-nav>li>a').on('click', function () {
+//     $('.navbar-collapse').collapse('hide');
+//   });
+
+//   // enabling expanded navbar to hide on click
+//   $('.navbar-brand').on('click', function () {
+//     $('.navbar-collapse').collapse('hide');
+//   });
